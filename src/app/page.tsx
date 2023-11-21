386c25bbcfa54ab89627fe7a0a6ba3ad70113b64
Vipin Singh <svipin174@gmail.com>
2023-11-21 10:00:10 +0530

    data fetching technique

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import delay from 'delay'
 
 const getData = async () => {
   await delay(2000)
-  return { data: [1, 2, 3] }
+  return { data: { title: 'Fist title' } }
 }
 
 export default async function Home() {
   const data = await getData()
-  console.log(data)
-  return <div>Hello World</div>
+  // console.log(data)
+  return <div>Hello {data.data.title}</div>
 }
---
+++ src/components/new-todo-form.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NewTodoForm = () => {
+  return <div>NewTodoForm</div>
+}
+
+export default NewTodoForm
