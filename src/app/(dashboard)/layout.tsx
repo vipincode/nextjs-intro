4f953d30ff2d6b9112942091f6e7d368a84dffc3
Vipin Singh <svipin174@gmail.com>
2023-11-21 09:43:57 +0530

    first commit

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import delay from 'delay'
+
+const getData = async () => {
+  await delay(2000)
+  return { data: [1, 2, 3] }
+}
+
+export default async function Home() {
+  const data = await getData()
+  console.log(data)
+  return <div>Hello World</div>
 }
